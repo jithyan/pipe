cc95d4947ffb3150856f5d8d265789b9046dcb63
s127014 <jithya.nanayakkara1@iag.com.au>
2023-12-15 12:59:40 +1100

    Add tests, fix async pipe

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,45 @@
 type AnyFunction = (...args: any[]) => any;
 
 type PipeInitalArgs<F extends Array<AnyFunction>> = F extends [
-    infer First,
-    ...infer Rest
+  infer First,
+  ...infer Rest,
 ]
-    ? First extends AnyFunction
-        ? Parameters<First>
-        : never
-    : Parameters<F[0]>;
+  ? First extends AnyFunction
+    ? Parameters<First>
+    : never
+  : Parameters<F[0]>;
 
 type PipeReturn<F extends Array<any> = AnyFunction[]> = F extends [
-    ...infer First,
-    infer Last
+  ...infer First,
+  infer Last,
 ]
-    ? Last extends AnyFunction
-        ? ReturnType<Last>
-        : never
-    : ReturnType<F[0]>;
+  ? Last extends AnyFunction
+    ? ReturnType<Last>
+    : never
+  : ReturnType<F[0]>;
 
 export function pipe<F extends Array<AnyFunction>>(
-    ...tasks: F
+  ...tasks: F
 ): (...initalArgs: PipeInitalArgs<F>) => PipeReturn<F> {
-    return (...initalArgs: PipeInitalArgs<F>) => {
-        return tasks.reduce(
-            (prev, curr) => curr(prev),
-            tasks[0]?.(initalArgs[0])
-        );
-    };
+  return (...initalArgs: PipeInitalArgs<F>) => {
+    let result = tasks[0]?.(...initalArgs);
+    for (let i = 1; i < tasks.length; i++) {
+      result = tasks[i]?.(result);
+    }
+    return result;
+  };
 }
 
-function perfPipe<F extends Array<AnyFunction>>(
-    ...tasks: F
-): (...initalArgs: PipeInitalArgs<F>) => PipeReturn<F> {
-    return (...initalArgs: PipeInitalArgs<F>) => {
-        let result = tasks[0]?.(...initalArgs);
-        for (let i = 1; i < tasks.length; i++) {
-            result = tasks[i]?.(result);
-        }
-        return result;
-    };
+type AnyAsyncFunction = (...args: any[]) => Promise<any>;
+
+export function asyncPipe<F extends Array<AnyAsyncFunction>>(
+  ...tasks: F
+): (...initalArgs: PipeInitalArgs<F>) => Promise<PipeReturn<F>> {
+  return async (...initalArgs: PipeInitalArgs<F>) => {
+    let result = await tasks[0]?.(...initalArgs);
+    for (let i = 1; i < tasks.length; i++) {
+      result = await tasks[i]?.(result);
+    }
+    return result;
+  };
 }
